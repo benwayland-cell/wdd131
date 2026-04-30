dce86440454704451977b30b5db9b9881ce687c5
benwayland-cell <ben.wayland2007@gmail.com>
2026-04-30 10:49:45 -0600

    Reduced nesting and imroved readability

--- ponder/W02/dom-theme.js
+++ ponder/W02/dom-theme.js
@@ -6,19 +6,37 @@ selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
     let current = selectElem.value;
-    if (current === 'ocean') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
-        pageContent.style.fontFamily = "Papyrus, fantasy";
-    } else if (current === 'forest') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
-        pageContent.style.fontFamily = "Impact, sans-serif";
-    } else if (current === 'desert') {
-        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
-        pageContent.style.fontFamily = "'Big Caslon', serif";
-    } else {
-        // default
-        document.body.style.backgroundImage = "none";
-        pageContent.style.fontFamily = "Georgia, serif";
+    switch (current) {
+        case 'ocean':
+            setToOceanTheme();
+            break;
+        case 'forest':
+            setToForestTheme();
+            break;
+        case 'desert':
+            setToDesertTheme();
+            break;
+        default:
+            setToDefaultTheme();
     }
 }
+
+function setToOceanTheme() {
+    document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
+    pageContent.style.fontFamily = "Papyrus, fantasy";
+}
+
+function setToForestTheme() {
+    document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
+    pageContent.style.fontFamily = "Impact, sans-serif";
+}
           
+function setToDesertTheme() {
+    document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
+    pageContent.style.fontFamily = "'Big Caslon', serif";
+}
+
+function setToDefaultTheme() {
+    document.body.style.backgroundImage = "none";
+    pageContent.style.fontFamily = "Georgia, serif";
+}
