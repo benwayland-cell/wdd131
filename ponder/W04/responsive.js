957d4993beb4730d53e494914db803b4dfd2a73f
benwayland-cell <ben.wayland2007@gmail.com>
2026-05-12 11:06:50 -0600

    Finished W04 Ponder: Responsive Menu

--- ponder/W04/responsive.js
+++ ponder/W04/responsive.js
@@ -1,2 +1,10 @@
 
-console.log("Hello World")
+let menuButton = document.querySelector(".menu-btn");
+let nav = document.querySelector("nav");
+
+menuButton.addEventListener('click', handleMenuButtonClick);
+
+function handleMenuButtonClick() {
+    nav.classList.toggle("show");
+    menuButton.classList.toggle("change");
+}
