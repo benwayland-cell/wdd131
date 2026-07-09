7d12142b421960c0660ea39741d00f8bc9bffde3
benwayland-cell <ben.wayland2007@gmail.com>
2026-07-08 19:21:40 -0600

    Can read user input

--- 2048/index.js
+++ 2048/index.js
@@ -1,6 +1,11 @@
 "use strict";
 // Elements
 const boardElement = document.getElementById("board");
+/**
+ * The board.
+ *
+ * The first index relates to the row (y) and the second relates to the column (x).
+ */
 let board = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -8,25 +13,59 @@ let board = [
     [0, 0, 0, 0]
 ];
 function init() {
-    board[1][2] = 2;
+    document.addEventListener("keydown", handleInput);
     renderBoard();
 }
+/**
+ * Renders the board onto the website
+ */
 function renderBoard() {
     if (boardElement == null) {
         return;
     }
+    /**
+     * Makes the HTML for the space given its value. 0 will not display.
+     * @param value The number the space has
+     * @returns The string in HTML form for a single space
+     */
     function spaceTemplate(value) {
         if (value == 0) {
             return `<div></div>`;
         }
         return `<div>${value}</div>`;
     }
+    // Clear the boardElement
     boardElement.innerHTML = "";
+    // Get the value at each space and add it to the boardElement
     for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board[row].length; col++) {
             const value = board[row][col];
             boardElement.innerHTML += spaceTemplate(value);
         }
+    }
+}
+/**
+ * Takes the user's keyboard press and takes action
+ * @param event The keyboard event
+ */
+function handleInput(event) {
+    switch (event.key) {
+        case "ArrowUp":
+        case "w":
+            console.log("up");
+            break;
+        case "ArrowDown":
+        case "s":
+            console.log("down");
+            break;
+        case "ArrowLeft":
+        case "a":
+            console.log("left");
+            break;
+        case "ArrowRight":
+        case "d":
+            console.log("right");
+            break;
     }
 }
 init();
--- 2048/index.ts
+++ 2048/index.ts
@@ -3,6 +3,11 @@
 const boardElement: HTMLElement | null = document.getElementById("board");
 
 
+/**
+ * The board.
+ * 
+ * The first index relates to the row (y) and the second relates to the column (x).
+ */
 let board: number[][] = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -10,16 +15,27 @@ let board: number[][] = [
     [0, 0, 0, 0]
 ];
 
-function init() {
-    board[1][2] = 2;
+
+function init(): void {
+    document.addEventListener("keydown", handleInput);
+
     renderBoard();
 }
 
+
+/**
+ * Renders the board onto the website
+ */
 function renderBoard(): void {
     if (boardElement == null) {
         return;
     }
 
+    /**
+     * Makes the HTML for the space given its value. 0 will not display.
+     * @param value The number the space has
+     * @returns The string in HTML form for a single space
+     */
     function spaceTemplate(value: number): string {
         if (value == 0) {
             return `<div></div>`
@@ -28,8 +44,10 @@ function renderBoard(): void {
         return `<div>${value}</div>`; 
     }
 
+    // Clear the boardElement
     boardElement.innerHTML = "";
 
+    // Get the value at each space and add it to the boardElement
     for (let row: number = 0; row < board.length; row++) {
         for (let col: number = 0; col < board[row].length; col++) {
             const value: number = board[row][col];
@@ -37,5 +55,35 @@ function renderBoard(): void {
         }
     }
 }
+
+
+/**
+ * Takes the user's keyboard press and takes action
+ * @param event The keyboard event
+ */
+function handleInput(event: KeyboardEvent): void {
+    switch (event.key) {
+        case "ArrowUp":
+        case "w":
+            console.log("up");
+            break;
+        
+        case "ArrowDown":
+        case "s":
+            console.log("down");
+            break;
+        
+        case "ArrowLeft":
+        case "a":
+            console.log("left");
+            break;
+        
+        case "ArrowRight":
+        case "d":
+            console.log("right");
+            break;
+    }
+}
+
 
 init();
